802882a241013c80e4c5f88db56a81140413e152
Alexey Klopov <aaklopov@yandex.ru>
2023-03-15 21:47:42 +0300

    Add files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,27 +2,16 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.opencsv.CSVReader;
-import com.opencsv.CSVWriter;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import java.io.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -30,15 +19,8 @@ import java.util.List;
 
 public class Main {
 
-
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, TransformerException {
         //# 1: csv_json парсер
-       // String[] employee = ("2,Ivan,Petrov,RU,23".split(","));
-       // try (CSVWriter writer = new CSVWriter(new FileWriter("data.csv", true))) {
-       //     writer.writeNext(employee);
-       // } catch (IOException e) {
-       //     e.printStackTrace();
-       // }
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
@@ -47,70 +29,32 @@ public class Main {
         writeString(json, jsonFilename);
 
         //# 2: xml_json парсер
-        //DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        //DocumentBuilder builder = factory.newDocumentBuilder();
-        //Document document = builder.newDocument();
-//
-        //Element root = document.createElement("root");
-        //document.appendChild(root);
-        //Element company = document.createElement("company");
-        //root.appendChild(company);
-        //Element staff = document.createElement("staff");
-        //company.appendChild(staff);
-        //Element employee = document.createElement("employee");
-        //employee.setAttribute("id", "1");
-        //employee.setAttribute("firstName", "John");
-        //employee.setAttribute("lastName", "Smith");
-        //employee.setAttribute("country", "USA");
-        //employee.setAttribute("age", "25");
-        //staff.appendChild(employee);
-        //Element employee1 = document.createElement("employee");
-        //employee1.setAttribute("id", "2");
-        //employee1.setAttribute("firstName", "Ivan");
-        //employee1.setAttribute("lastName", "Petrov");
-        //employee1.setAttribute("country", "RU");
-        //employee1.setAttribute("age", "23");
-        //staff.appendChild(employee1);
-//
-        //DOMSource domSource = new DOMSource(document);
-        //StreamResult streamResult = new StreamResult(new File("data.xml"));
-//
-        //TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        //Transformer transformer = transformerFactory.newTransformer();
-        //transformer.transform(domSource, streamResult);
-
-        String xmlFilename = "data.xml";
-        List<Employee> list2 = parseXML(xmlFilename);
+        String xmlFileName = "data.xml";
+        List<Employee> list2 = parseXML(xmlFileName);
         String json2 = listToJson(list2);
         String jsonFilename2 = "data2.json";
         writeString(json2, jsonFilename2);
     }
-    private static List<Employee> parseXML(String xmlFilename) throws ParserConfigurationException,
+
+    private static List<Employee> parseXML(String xmlFileName) throws ParserConfigurationException,
             IOException, SAXException {
-        List<String> elements = new ArrayList<>();
         List<Employee> list = new ArrayList<>();
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse(new File(xmlFilename));
-        Node root = document.getDocumentElement();
-        NodeList nodeList = root.getChildNodes();
-        for (int i = 0; i < nodeList.getLength(); i++) {
+        Document document = builder.parse(new File(xmlFileName));
+        Node staff = document.getDocumentElement();
+        NodeList nodeList = staff.getChildNodes();
+        for (int i = 1; i < nodeList.getLength(); i++) {
             Node node = nodeList.item(i);
-            if (node.getNodeName().equals("employee")) {
-                NodeList nodeList1 = node.getChildNodes();
-                for (int j = 0; j < nodeList1.getLength(); j++) {
-                    Node node_ = nodeList1.item(j);
-                    if (Node.ELEMENT_NODE == node_.getNodeType()) {
-                        elements.add(node_.getTextContent());
-                    }
-                }
+            if (Node.ELEMENT_NODE == node.getNodeType()) {
+                Element element = (Element) node;
                 list.add(new Employee(
-                        Long.parseLong(elements.get(0)),
-                        elements.get(1),
-                        elements.get(2),
-                        elements.get(3),
-                        Integer.parseInt(elements.get(4))));
-                elements.clear();
+                        Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent()),
+                        element.getElementsByTagName("firstName").item(0).getTextContent(),
+                        element.getElementsByTagName("lastName").item(0).getTextContent(),
+                        element.getElementsByTagName("country").item(0).getTextContent(),
+                        Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent())
+                ));
             }
         }
         return list;
